b4cd63d4cb927ed3e75a3272ad51ab6aca99755d
John Eastwood <jjge732@gmail.com>
2019-01-16 20:49:04 -0600

    fix heroku

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,4 +4,9 @@ module.exports = (app, db) => {
     app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, '/../public/index.html'));
     });
+    if(process.env.NODE_ENV === 'production') {
+        app.get('/', (req, res) => {
+            res.sendFile(path.join(__dirname, '/../build/index.html'));
+        });
+    }
 }
