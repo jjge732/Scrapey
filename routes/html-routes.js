ed550bccc3cffe2f1e5e95a586538c75137db86c
John Eastwood <jjge732@gmail.com>
2019-01-16 21:06:11 -0600

    changed routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,11 +2,11 @@ const path = require('path');
 
 module.exports = (app, db) => {
     app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '/../public/index.html'));
+        res.sendFile(path.join(__dirname, '/react-folder/public/index.html'));
     });
     if(process.env.NODE_ENV === 'production') {
         app.get('/', (req, res) => {
-            res.sendFile(path.join(__dirname, '/../build/index.html'));
+            res.sendFile(path.join(__dirname, '/react-folder/build/index.html'));
         });
     }
 }
