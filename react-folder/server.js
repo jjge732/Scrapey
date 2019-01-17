2680785dc3ae2295d5d43c17eb485681a16607a5
John Eastwood <jjge732@gmail.com>
2019-01-16 19:48:30 -0600

    added production static file path

--- react-folder/server.js
+++ react-folder/server.js
@@ -8,7 +8,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '/app/public')));
+app.use(express.static(path.join(__dirname, '/react-folder/public')));
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'react-folder/build')));
+}
 
 require('./routes/html-routes.js')(app, db);
 require('./routes/api-routes.js')(app, db);
