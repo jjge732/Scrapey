c288ee9c53f5ada492bf99bd58eedb43f76d9d70
John Eastwood <jjge732@gmail.com>
2019-01-16 21:12:49 -0600

    one more try...

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,12 +1,12 @@
 const path = require('path');
 
 module.exports = (app, db) => {
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '/react-folder/public/index.html'));
-    });
-    if(process.env.NODE_ENV === 'production') {
+    // app.get('/', (req, res) => {
+    //     res.sendFile(path.join(__dirname, '/react-folder/public/index.html'));
+    // });
+    // if(process.env.NODE_ENV === 'production') {
         app.get('/', (req, res) => {
             res.sendFile(path.join(__dirname, '/react-folder/build/index.html'));
         });
-    }
+    // }
 }
